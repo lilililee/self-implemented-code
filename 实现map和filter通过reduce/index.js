b526b0ccab6e67a4f5f6d7ed029fbafd709b7548
lilililee <liguang_hello@icould.com>
2019-04-12 17:57:44 +0800

    #update 新增map和filter指定this功能

--- 实现map和filter通过reduce/index.js
+++ 实现map和filter通过reduce/index.js
@@ -1,9 +1,9 @@
 // 需要将下面方便绑定到 Function.prototype 测试
-function _map (fn) {
-  return this.reduce((result, item) => [...result, fn(item)], [])
+function _map (fn, ctx = null) {
+  return this.reduce((result, item) => [...result, fn.call(ctx, item)], [])
 }
-function _filter (fn) {
-  return this.reduce((result, item) => (fn(item) ? [...result, item] : result), [])
+function _filter (fn, ctx = null) {
+  return this.reduce((result, item) => (fn.call(ctx, item) ? [...result, item] : result), [])
 }
 
 module.exports = {
